60e8cc4d6042e08fade4cf2c5e2bba7b01b1914c
ZackRohde <zdrohde@outlook.com>
2022-09-30 14:28:59 -0700

    Initial Commit

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,22 +16,43 @@
 // a) Create a test with an expect statement using the variable provided. 
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
+describe("shuffleArray", () => {
+  it("takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+    expect(shuffleArray(colors1)).toEqual(expect.arrayContaining["yellow", "blue", "pink", "green"])
+    expect(shuffleArray(colors2)).toEqual(expect.arrayContaining["saffron", "aquamarine", "periwinkle", "indigo", "ochre"])
+  })
+})
+
+// Good Fail!
+// ReferenceError: shuffleArray is not defined
+
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
-
 // b) Create the function that makes the test pass.
 
+// PSUEDO CODE:
+// Create a function named shuffleArray
+// .shift(1,5) to remove the first item from the array
+// I need to figure out how to shuffle the array after I slice purple from 
+// console log to check if it's working through node 
+// yarn jest to test my pass or fail
+
+const shuffleArray = () => {
+  let shuffledArray = colors1.sort(() => Math.random() - 0.5)
+  return colors1.slice(1, 5)
+}
+console.log(shuffleArray(colors1))
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the net total of votes.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const votes1 = {upVotes: 13, downVotes: 2}
+const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
-const votes2 = {upVotes: 2, downVotes: 33}
+const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
 
